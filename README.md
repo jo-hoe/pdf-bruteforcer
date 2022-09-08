3491ed26d9b690e4e23e4311dd695212a1d8852d
Johannes Hoenger <johannes.hoenger@gmail.com>
2022-09-08 12:26:02 +0200

    added documentation and parameterized filename

--- README.md
+++ README.md
@@ -1,4 +1,20 @@
 # pdf-bruteforcer-demo
+
 [![Test Status](https://github.com/jo-hoe/pdf-bruteforcer/workflows/unittests/badge.svg)](https://github.com/jo-hoe/pdf-bruteforcer/actions?workflow=unittests)
 
 Demonstrates how a password secured pdf can be cracked
+
+## Environment
+
+This is a python 3 project.
+You may create a virtual python environment like so:
+
+```bash
+python -m venv .venv
+```
+
+and afterwards install your dependencies:
+
+```bash
+.\.venv\Scripts\pip.exe install -r requirements.txt
+```
--- demo.py
+++ demo.py
@@ -1,9 +1,21 @@
 from datetime import datetime
+import sys
 from src.passwordgenerator import DatesGenerator
 from src.pdfbruteforcer import PdfBruteForcer
 from datetime import datetime
 
-generator = DatesGenerator(datetime(1980, 1, 1),datetime(2020, 1, 1), "%d%m%Y")
-bruteforcer = PdfBruteForcer("demopath", generator)
+generator = DatesGenerator(datetime(1990, 1, 1),datetime(2020, 1, 1), "%d%m%Y")
+
+# set default name for file
+filename = "password-protected-document.pdf"
+if len(sys.argv) > 1:
+    filename = sys.argv[1]
+
+bruteforcer = PdfBruteForcer("password-protected-document.pdf", generator)
+print("starting bruteforce attack...")
 password = bruteforcer.bruteforce()
-print("Found the password: " + password)
+
+if password == None:
+    print("did not find the password")
+else:
+    print("password is: " + password)
